6a7e1fd7439c0847c77bb4e56cd84f6cc255a0cb
cpenuela19 <ce.penuela20@uniandes.edu.co>
2024-11-10 17:57:15 -0500

    v1

--- src/museum/museum.module.ts
+++ src/museum/museum.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MuseumService } from './museum.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MuseumEntity } from './museum.entity';
 
-@Module({})
+@Module({
+  providers: [MuseumService],
+  imports: [TypeOrmModule.forFeature([MuseumEntity])],
+})
 export class MuseumModule {}
